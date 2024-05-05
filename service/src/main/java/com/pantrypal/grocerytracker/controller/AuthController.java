9b23d75d0f1d9a78f67d9a70fe76fba6a27f30ec
erisjacey-MACBOOK <eris_jacey@hotmail.com>
2024-05-05 20:58:59 +0800

    Complete swagger annotations for AuthController

--- service/src/main/java/com/pantrypal/grocerytracker/controller/AuthController.java
+++ service/src/main/java/com/pantrypal/grocerytracker/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.pantrypal.grocerytracker.dto.auth.AuthLoginRequest;
 import com.pantrypal.grocerytracker.dto.auth.AuthRegisterRequest;
 import com.pantrypal.grocerytracker.dto.auth.AuthResponse;
 import com.pantrypal.grocerytracker.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -24,14 +25,24 @@ public class AuthController {
         this.authService = authService;
     }
 
+    @Operation(summary = "Log a user in")
     @PostMapping(Constants.AUTH_API_LOGIN)
-    public ResponseEntity<?> login(@RequestBody AuthLoginRequest request) {
+    public ResponseEntity<?> login(
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Login details of the user"
+            ) @RequestBody AuthLoginRequest request
+    ) {
         AuthResponse response = authService.logUserIn(request);
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "Register a user")
     @PostMapping(Constants.AUTH_API_REGISTER)
-    public ResponseEntity<?> register(@RequestBody AuthRegisterRequest request) {
+    public ResponseEntity<?> register(
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Registration details of the user"
+            ) @RequestBody AuthRegisterRequest request
+    ) {
         AuthResponse response = authService.registerUser(request);
         return ResponseEntity.ok(response);
     }
