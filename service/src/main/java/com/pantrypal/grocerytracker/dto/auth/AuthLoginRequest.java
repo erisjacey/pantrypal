2429f045da2fdbc9d40a8ba7954bfc7bd8fc17a0
erisjacey-MACBOOK <eris_jacey@hotmail.com>
2024-07-07 16:19:49 +0800

    Add javadoc for auth DTOs

--- service/src/main/java/com/pantrypal/grocerytracker/dto/auth/AuthLoginRequest.java
+++ service/src/main/java/com/pantrypal/grocerytracker/dto/auth/AuthLoginRequest.java
@@ -3,6 +3,10 @@ package com.pantrypal.grocerytracker.dto.auth;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+/**
+ * Represents a login request extending {@link AuthRequest}.
+ * Inherits username and password fields from {@link AuthRequest}.
+ */
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class AuthLoginRequest extends AuthRequest {
